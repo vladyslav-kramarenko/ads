e8b3fa2ca812fcbaef885c15edef75fb32934753
vladyslav-kramarenko <razorvlad1992@gmail.com>
2024-11-10 07:46:41 -0800

    added localization

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,43 @@
 import React from 'react';
+import { BrowserRouter as Router, Routes, Route, Navigate, useParams } from 'react-router-dom';
+import Header from './components/Header/Header';
+import Quiz from './components/Quiz/Quiz';
+import About from './components/About/About';
+import i18n from './i18n';
+import Advantages from "./components/Advantages/Advantages";
+
+const LanguageWrapper: React.FC = () => {
+    const { lang } = useParams<{ lang: string }>();
+
+    // Change language based on the URL parameter
+    React.useEffect(() => {
+        if (lang) {
+            i18n.changeLanguage(lang);
+        }
+    }, [lang]);
+
+    return (
+        <>
+            <Header />
+            <Quiz />
+            <About />
+            <Advantages/>
+        </>
+    );
+};
 
 const App: React.FC = () => {
-  return (
-      <div className="App">
-        <Header />
-        <Quiz />
-        <About />
-        <Advantages />
-      </div>
-  );
+    return (
+        <Router>
+            <Routes>
+                {/* Redirect from the root to English by default */}
+                <Route path="/" element={<Navigate replace to="/en" />} />
+
+                {/* Language-specific routes */}
+                <Route path="/:lang" element={<LanguageWrapper />} />
+            </Routes>
+        </Router>
+    );
 };
 
 export default App;
