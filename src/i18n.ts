1ef82dc6d305e7254969603b06a214074c1109b7
vladyslav-kramarenko <razorvlad1992@gmail.com>
2024-11-10 07:46:15 -0800

    added localization

---
+++ src/i18n.ts
@@ -0,0 +1,23 @@
+// src/i18n.ts
+import i18n from 'i18next';
+import { initReactI18next } from 'react-i18next';
+import en from './locales/en.json';
+import ru from './locales/ru.json';
+import tr from './locales/tr.json';
+import ua from './locales/ua.json';
+
+i18n.use(initReactI18next).init({
+    resources: {
+        en: { translation: en },
+        ru: { translation: ru },
+        tr: { translation: tr },
+        ua: { translation: ua },
+    },
+    lng: 'en',
+    fallbackLng: 'en',
+    interpolation: {
+        escapeValue: false,
+    },
+});
+
+export default i18n;
