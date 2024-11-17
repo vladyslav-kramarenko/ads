aed2ac7946731d99996f6db7746926a015f1a59c
vladyslav-kramarenko <razorvlad1992@gmail.com>
2024-11-17 11:38:04 -0800

    styles change

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -5,26 +5,25 @@ import {useTranslation} from "react-i18next";
 const Header: React.FC = () => {
     const {t} = useTranslation();
     return (
-        <header className={styles.header}>
-            <div className={styles.logo}>
-                <img src="img/desire-logo.svg" alt="DESIRE Logo"/>
-            </div>
-            <div className={styles.headerText}>
-                <h1 className={styles.title}>{t('header.title')}</h1>
-                <span className={styles.subtitle}>
+        <header className={styles.headerWrapper}>
+            <div className={styles.header}>
+                <div className={styles.logo}>
+                    <img src="img/desire-logo.svg" alt="DESIRE Logo"/>
+                </div>
+                <div className={styles.headerText}>
+                    <h1 className={styles.title}>{t('header.title')}</h1>
+                    <span className={styles.subtitle}>
                     <p className={"accent"}>{t('header.subTitle')}</p>
                 </span>
-                <p className={styles.description}>
-                    DESIRE <br/> Premium Hotel-Residential Complex
-                    <br/>
-                    (Antalya, Turkey)
-                </p>
-                <a className={"orangeButton"} href={"#quiz"}>Leave a request</a>
-                {/*<button className={"orangeButton"}>*/}
-                {/*    Leave a request*/}
-                {/*</button>*/}
+                    <p className={styles.description}>
+                        DESIRE <br/> Premium Hotel-Residential Complex
+                        <br/>
+                        (Antalya, Turkey)
+                    </p>
+                    <a className={"orangeButton"} href={"#quiz"}>Leave a request</a>
+                </div>
             </div>
-            <img className="sectionDividerImg" src={"img/img.png"}/>
+            <img className={styles.sectionDividerImg} src={"img/img.png"}/>
         </header>
     );
 };
